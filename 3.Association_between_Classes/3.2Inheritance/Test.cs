1c8e686620de7d839edcd571a41445f02110405d
PrakhanRattakool <56579980+PrakhanRattakool@users.noreply.github.com>
2019-10-20 15:31:47 +0700

    3.2 UpdateText

--- 3.Association_between_Classes/3.2Inheritance/Test.cs
+++ 3.Association_between_Classes/3.2Inheritance/Test.cs
@@ -1,7 +1,13 @@
 ﻿namespace Inheritance
 {
-    public class Test
+    public class Test : PersentationObject
     {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
 
+        public void AddHyperlink(string url)
+        {
+            System.Console.WriteLine("We added a link to " + url);
+        }
     }
 }
